5b25fbaa83380402a9a2afb9239e21dc511e07d7
gunn3r71 <lucasgunner3@gmail.com>
2021-05-04 23:59:44 -0300

    Adicionando funcionalidade sem refactoring aumentando complexidade

--- SistemaNotificacao/Program.cs
+++ SistemaNotificacao/Program.cs
@@ -11,11 +11,27 @@ namespace SistemaNotificacao
             int codCliente = int.Parse(Console.ReadLine());
             Console.Write("Digite a mensagem a ser enviada: ");
             string mensagem = Console.ReadLine();
+            Console.WriteLine("Tipo de mensagem a ser enviada:\n1 Para Email\n2 Para Mensagem de texto");
+            int opcao = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Enviando email para cliente de código {codCliente}...");
-            Console.WriteLine(".....Aguarde");
-            Console.WriteLine($"Email enviado: {mensagem}");
-            Console.WriteLine("Email Enviado com sucesso");
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine($"Enviando email para cliente de código {codCliente}...");
+                    Console.WriteLine(".....Aguarde");
+                    Console.WriteLine($"Email enviado: {mensagem}");
+                    Console.WriteLine("Email Enviado com sucesso");
+                    break;
+                case 2:
+                    Console.WriteLine($"Enviando mensagem para cliente de código {codCliente}...");
+                    Console.WriteLine(".....Aguarde");
+                    Console.WriteLine($"mensagem enviada: {mensagem}");
+                    Console.WriteLine("mensagem Enviada com sucesso");
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
