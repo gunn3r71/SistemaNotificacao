8c186ea223dcfc677b84d31c86e488cd77fa19f0
gunn3r71 <lucasgunner3@gmail.com>
2021-05-04 23:38:15 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaNotificacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaNotificacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
