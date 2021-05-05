ddb47a614b2a2adbd9a96dd5f216909a7e3296a3
gunn3r71 <lucasgunner3@gmail.com>
2021-05-04 23:50:08 -0300

    Sistema com implementação básica na classe de view

--- SistemaNotificacao/Program.cs
+++ SistemaNotificacao/Program.cs
@@ -6,7 +6,16 @@ namespace SistemaNotificacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("####Sistema de notificação####");
+            Console.Write("Digite o código do cliente: ");
+            int codCliente = int.Parse(Console.ReadLine());
+            Console.Write("Digite a mensagem a ser enviada: ");
+            string mensagem = Console.ReadLine();
+
+            Console.WriteLine($"Enviando email para cliente de código {codCliente}...");
+            Console.WriteLine(".....Aguarde");
+            Console.WriteLine($"Email enviado: {mensagem}");
+            Console.WriteLine("Email Enviado com sucesso");
         }
     }
 }
